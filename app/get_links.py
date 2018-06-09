dea24469edd7fbd78dda30ad95f3dfca99c6265b
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-06-10 00:15:35 +0300

    Update get links script

--- app/get_links.py
+++ app/get_links.py
@@ -1,5 +1,6 @@
 import unittest
 import re
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common import proxy
@@ -17,18 +18,18 @@ class TestRosAccreditationSite(unittest.TestCase):
         proxy_url = '{protocol}://{host}:{port}'.format(protocol=proxy_protocol, host=proxy_host, port=proxy_port)
 
         self.options = webdriver.ChromeOptions()
-        # self.options.add_argument('--proxy-server={url}'.format(url=proxy_url))
         self.selenium_grid_url = 'http://188.246.227.206:4444/wd/hub'
 
 
     def test_proxy(self):
         driver = None
         try:
+            # self.options.add_argument('--proxy-server={url}'.format(url=proxy_url))
             driver = webdriver.Remote(command_executor=self.selenium_grid_url, desired_capabilities=self.options.to_capabilities())
 
             page_url = 'http://public.fsa.gov.ru/table_rds_pub_ts/'
             driver.get(page_url)
-            wait = WebDriverWait(driver, 3*60)
+            wait = WebDriverWait(driver, 30*60)
             btn_find = wait.until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '#btn_find'))
             )
@@ -39,11 +40,7 @@ class TestRosAccreditationSite(unittest.TestCase):
                 EC.presence_of_element_located((By.CSS_SELECTOR, '#ContainerGrid'))
             )
 
-            driver.get_screenshot_as_file('./container_grid_page_1.png')
-
-            # driver.execute_script('window.tableManager.changePerPage(3000)')
-
-            long_wait = WebDriverWait(driver, 7*60)
+            long_wait = WebDriverWait(driver, 30*60)
             cl_last = long_wait.until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '.cl_last'))
             )
@@ -54,18 +51,20 @@ class TestRosAccreditationSite(unittest.TestCase):
             result = re.search(regexp, onclick, re.I | re.U)
             last_page = result.group('last_page')
             print(last_page)
+            script = """downloadPage('index.php',
+                                        'ajax=main&' + tableManager.getControlsData() + '&idid_=content-table'+getDivContent('tableContent-content-table')+
+                                        '&page_byid_=100&page_noid_={page_id}',
+                                        'tableContent-content-table');"""
 
-            for page_id in range(2, int(last_page) + 1):
-                next_page = long_wait.until(
-                    EC.presence_of_element_located((By.XPATH, "//span[contains(@class, '') and text() = '{page_id}']".format(page_id=page_id)))
-                )
-
-                next_page.click()
-
+            for page_id in range(0, int(last_page)):
+                driver.execute_script(script.format(page_id=page_id))
                 container_grid = long_wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '#ContainerGrid'))
+                    EC.presence_of_element_located((By.CSS_SELECTOR, '.cl_navigPage'))
                 )
-
+                driver.get_screenshot_as_file('./checkpoint.png')
+                links = driver.find_elements_by_css_selector('.dl_cert_num.object.link')
+                links = [link.get_attribute('href') for link in links]
+                print(page_id, ' => ', len(links))
                 driver.get_screenshot_as_file('./container_grid_page_{}.png'.format(page_id))
 
             print('Done!')
