a3b517b38516d3af10c0831126140d067003ef47
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-06-10 13:23:13 +0300

    Update links parser, add db insets

--- app/get_links.py
+++ app/get_links.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+DOTENV_PATH = os.path.join(BASE_PATH, '.env')
+load_dotenv(DOTENV_PATH)
+
 import unittest
 import re
 import time
+import psycopg2
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.common import proxy
@@ -12,6 +21,16 @@ from selenium.webdriver.support import expected_conditions as EC
 class TestRosAccreditationSite(unittest.TestCase):
 
     def setUp(self):
+        # logger
+        self.logger = logging.getLogger(__name__)
+        logger_handler = logging.FileHandler(os.path.join(BASE_PATH, '{}.log'.format(__file__)))
+        logger_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        logger_handler.setFormatter(logger_formatter)
+        self.logger.addHandler(logger_handler)
+        self.logger.setLevel(logging.WARNING)
+        self.logger.propagate = False
+
+        # options
         proxy_protocol = 'http'
         proxy_host = '212.237.53.59'
         proxy_port = '3128'
@@ -20,8 +39,13 @@ class TestRosAccreditationSite(unittest.TestCase):
         self.options = webdriver.ChromeOptions()
         self.selenium_grid_url = 'http://188.246.227.206:4444/wd/hub'
 
+    def save_links(self):
+        PS_DB_NAME = os.getenv('PS_DB_NAME', '')
+        PS_USER = os.getenv('PS_USER', '')
+        PS_PASSWORD = os.getenv('PS_PASSWORD', '')
+        PS_HOST = os.getenv('PS_HOST', 'localhost')
+        PS_PORT = os.getenv('PS_PORT', 5432)
 
-    def test_proxy(self):
         driver = None
         try:
             # self.options.add_argument('--proxy-server={url}'.format(url=proxy_url))
@@ -51,21 +75,50 @@ class TestRosAccreditationSite(unittest.TestCase):
             result = re.search(regexp, onclick, re.I | re.U)
             last_page = result.group('last_page')
             print(last_page)
-            script = """downloadPage('index.php',
-                                        'ajax=main&' + tableManager.getControlsData() + '&idid_=content-table'+getDivContent('tableContent-content-table')+
-                                        '&page_byid_=100&page_noid_={page_id}',
-                                        'tableContent-content-table');"""
 
-            for page_id in range(0, int(last_page)):
-                driver.execute_script(script.format(page_id=page_id))
-                container_grid = long_wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '.cl_navigPage'))
-                )
-                driver.get_screenshot_as_file('./checkpoint.png')
-                links = driver.find_elements_by_css_selector('.dl_cert_num.object.link')
-                links = [link.get_attribute('href') for link in links]
-                print(page_id, ' => ', len(links))
-                driver.get_screenshot_as_file('./container_grid_page_{}.png'.format(page_id))
+
+            with psycopg2.connect(dbname=PS_DB_NAME, user=PS_USER, password=PS_PASSWORD, host=PS_HOST, port=PS_PORT) as connection:
+                with connection.cursor() as cursor:
+                    sql_string = """
+                        SELECT "page"
+                        FROM "declaration"
+                        GROUP BY "page"
+                        ORDER BY "page";
+                    """
+                    cursor.execute(sql_string)
+
+                    pages = [row[0] for row in cursor.fetchall()]
+                    script = """downloadPage('index.php',
+                        'ajax=main&' + tableManager.getControlsData() + '&idid_=content-table'+getDivContent('tableContent-content-table')+
+                        '&page_byid_=100&page_noid_={page_id}',
+                        'tableContent-content-table');
+                    """
+
+                    for page_id in range(0, int(last_page)):
+                        if page_id not in pages:
+                            driver.execute_script(script.format(page_id=page_id))
+                            container_grid = long_wait.until(
+                                EC.presence_of_element_located((By.CSS_SELECTOR, '.cl_navigPage'))
+                            )
+                            driver.get_screenshot_as_file('./checkpoint.png')
+                            links = driver.find_elements_by_css_selector('.dl_cert_num.object.link')
+                            links = [link.get_attribute('href') for link in links]
+                            print(page_id, ' => ', len(links))
+                            # driver.get_screenshot_as_file('./container_grid_page_{}.png'.format(page_id))
+
+                            values = []
+                            for link in links:
+                                values.append("('{link}', {page_id})".format(link=link, page_id=page_id))
+                            values = ", ".join(values)
+
+                            sql_string = """
+                                INSERT INTO "declaration" ("url", "page")
+                                VALUES {values};
+                            """.format(values=values)
+                            cursor.execute(sql_string)
+                            connection.commit()
+                        else:
+                            print('Page {page_id} already done!'.format(page_id=page_id))
 
             print('Done!')
         except (KeyboardInterrupt, Exception) as e:
@@ -76,6 +129,9 @@ class TestRosAccreditationSite(unittest.TestCase):
             print('quit')
             if driver:
                 driver.quit()
+
+    def test_proxy(self):
+        self.save_links()
 
 if __name__ == '__main__':
     unittest.main()
